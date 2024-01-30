c7e2cf19a4c8c6045302a01b9a894ca0aee429c5
Matt Gacy <matt@mgacy.com>
2024-01-30 12:21:56 -0700

    Implement models

--- Sources/PackageManifestGeneratorCore/Models/Product.swift
+++ Sources/PackageManifestGeneratorCore/Models/Product.swift
@@ -9,4 +9,16 @@ import Foundation
 
 /// A Swift package product.
 struct Product: Equatable {
+    
+    enum ProductType: Codable, Equatable {
+        case executable
+        case library
+        case dynamicLibrary
+        case staticLibrary
+        case plugin
+    }
+
+    let name: String
+    let type: ProductType
+    let targets: [String]
 }
--- Sources/PackageManifestGeneratorCore/Models/Target.swift
+++ Sources/PackageManifestGeneratorCore/Models/Target.swift
@@ -9,7 +9,41 @@ import Foundation
 
 /// A Swift package target.
 struct Target: Equatable {
+
+    struct Resource: Equatable {
+
+        enum Rule: Equatable {
+            case copy
+            case process(_ localization: String?)
+        }
+
+        let rule: Rule
+        let path: String
+    }
+
+    enum TargetType: String {
+        case regular
+        case executable
+        case test
+    }
+
+    struct PluginUsage: Equatable {
+        let name: String
+        let package: String?
+    }
+
+    public enum Dependency: Equatable {
+    }
+
     /// The name of the target.
     let name: String
 
+    let path: String?
+    let sources: [String]?
+    let resources: [Resource]?
+    let exclude: [String]
+    let dependencies: [Dependency]
+    let type: TargetType
+    let packageAccess: Bool
+    let plugins: [PluginUsage]?
 }
