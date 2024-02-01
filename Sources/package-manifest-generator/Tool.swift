97feeebe75766e6b8bf9745fce0654b5ea111d4f
Matt Gacy <matt@mgacy.com>
2024-02-01 09:41:22 -0700

    Add, use version file

--- Sources/package-manifest-generator/Tool.swift
+++ Sources/package-manifest-generator/Tool.swift
@@ -8,8 +8,14 @@
 import ArgumentParser
 import PackageManifestGeneratorCore
 
+/// The main entry point for `package-manifest-generator`.
 @main
 struct Tool: AsyncParsableCommand {
+    static let configuration = CommandConfiguration(
+        abstract: "Generate Swift package manifest products and targets from configuration files.",
+        version: Version.number)
+
+    /// Runs the tool.
     mutating func run() async throws {
         try await PackageManifestGenerator.run(packagePath: "")
     }
