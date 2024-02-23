10062cf365d6743574b9ffa8c66aafa37c9d64e3
Matt Gacy <matt@mgacy.com>
2024-02-23 14:12:49 -0700

    Add tests for CodableDefault

---
+++ Tests/PackageManifestGeneratorTests/CodableDefaultTests.swift
@@ -0,0 +1,60 @@
+//
+//  CodableDefaultTests.swift
+//  PackageManifestGenerator
+//
+//  Created by Mathew Gacy on 2/23/24.
+//
+
+@testable import PackageManifestGeneratorCore
+import Foundation
+import XCTest
+
+final class CodableDefaultTests: XCTestCase {
+    struct TestModel: Codable, Equatable {
+        @CodableDefault<Empty> var string: String
+        @CodableDefault<EmptyDictionary> var defaultDictionary: [String: String]
+        @CodableDefault<False> var falseBool: Bool
+        @CodableDefault<True> var trueBool: Bool
+
+        init(
+            string: String,
+            defaultDictionary: [String : String],
+            falseBool: Bool,
+            trueBool: Bool
+        ) {
+            self.string = string
+            self.defaultDictionary = defaultDictionary
+            self.falseBool = falseBool
+            self.trueBool = trueBool
+        }
+
+        static let `default` = TestModel(
+            string: "",
+            defaultDictionary: [:],
+            falseBool: false,
+            trueBool: true)
+    }
+
+    let decoder = JSONDecoder()
+    let encoder = JSONEncoder()
+
+    func decode<T: Decodable>(_ type: T.Type, from json: String) throws -> T {
+        try decoder.decode(type.self, from: Data(json.utf8))
+    }
+
+    func encode<T: Encodable>(_ value: T) throws -> String {
+        String(decoding: try encoder.encode(value), as: UTF8.self)
+    }
+}
+
+extension CodableDefaultTests {
+    func testDecodeDefault() throws {
+        let actual = try decode(TestModel.self, from: "{}")
+        XCTAssertEqual(actual, .default)
+    }
+
+    func testEncodeDefault() throws {
+        let actual = try encode(TestModel.default)
+        XCTAssertEqual(actual, "{}")
+    }
+}
